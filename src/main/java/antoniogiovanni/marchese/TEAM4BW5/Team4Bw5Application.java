324f95b211361ff3c9fe111e0a70cf0e5ef2d611
marchese89 <marchese89@hotmail.com>
2024-01-29 10:49:45 +0100

    Initial commit

---
+++ src/main/java/antoniogiovanni/marchese/TEAM4BW5/Team4Bw5Application.java
@@ -0,0 +1,13 @@
+package antoniogiovanni.marchese.TEAM4BW5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4Bw5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4Bw5Application.class, args);
+	}
+
+}
---
+++ src/test/java/antoniogiovanni/marchese/TEAM4BW5/Team4Bw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package antoniogiovanni.marchese.TEAM4BW5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4Bw5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
